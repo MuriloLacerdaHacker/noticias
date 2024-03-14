342e0ed49b96338d2d3f6f85dd45baf9ed26f95e
Murilo Lacerda <murilolopes1308@gmail.com>
2024-03-14 19:49:18 -0300

    Criação da Navegação desktop, remoção da classe header mobile e inicio da responsividade

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-const botaoAbrir = document.querySelector(".header-mobile > button")
-const botaoFechar = document.querySelector(".header-mobile nav button")
+const botaoAbrir = document.querySelector("header > button")
+const botaoFechar = document.querySelector("header nav button")
 
 const body = document.querySelector("body")
-const nav = document.querySelector(".header-mobile nav")
-
-console.log(body)
+const nav = document.querySelector("header .mobile")
 
 botaoAbrir.addEventListener('click', abrirMenu)
 botaoFechar.addEventListener('click', fecharMenu)
@@ -15,6 +13,6 @@ function abrirMenu() {
 }
 
 function fecharMenu() {
-    body.classList.add("escurecer")
-    nav.classList.add("abrir")
+    body.classList.remove("escurecer")
+    nav.classList.remove("abrir")
 }
